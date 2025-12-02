23860dcb491ff6bd6cf9675a81d42e2a92f09763
Rahul B <rahul.b@tahrtech.com>
2025-12-02 15:17:22 +0530

    added the JPA and Mysql Connection

--- src/main/java/com/scaler/ecommerce/model/Category.java
+++ src/main/java/com/scaler/ecommerce/model/Category.java
@@ -1,5 +1,8 @@
 package com.scaler.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +10,12 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity(name = "categories")
 public class Category extends  BaseModel {
 
     private String name;
     private String description;
+    @OneToMany(mappedBy = "category")
     List<Product> products;
 
 }
--- src/main/java/com/scaler/ecommerce/model/Product.java
+++ src/main/java/com/scaler/ecommerce/model/Product.java
@@ -1,5 +1,9 @@
 package com.scaler.ecommerce.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,12 +11,14 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity(name = "products")
 public class Product extends BaseModel {
 
     private String name;
     private String description;
     private Double price;
     private String imageUrl;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
 }
